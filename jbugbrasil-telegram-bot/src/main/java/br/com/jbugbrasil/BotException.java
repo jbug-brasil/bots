0d8f18a78a5275a886f01bb01cd1d83f36fd4f19
Ataxexe <ataxexe@devnull.tools>
2016-07-28 21:48:03 -0300

    refactor

---
+++ jbugbrasil-telegram-bot/src/main/java/br/com/jbugbrasil/BotException.java
@@ -0,0 +1,24 @@
+package br.com.jbugbrasil;
+
+public class BotException extends RuntimeException {
+
+  public BotException() {
+  }
+
+  public BotException(String message) {
+    super(message);
+  }
+
+  public BotException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public BotException(Throwable cause) {
+    super(cause);
+  }
+
+  public BotException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+
+}
---
+++ jbugbrasil-telegram-bot/src/main/java/br/com/jbugbrasil/Component.java
@@ -0,0 +1,7 @@
+package br.com.jbugbrasil;
+
+public interface Component {
+
+  void initialize();
+
+}
