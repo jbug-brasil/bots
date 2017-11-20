4c1db6d54fb3695a125d3a571777cbc8ff953767
Filippe Spolti <fspolti@redhat.com>
2017-11-20 16:08:20 -0200

    Criaçaõ de um cache default para comum pela api, plugins/serviços/comandos do bot

---
+++ jbugbrasil-telegram-bot/services/cache-service/src/main/java/br/com/jbugbrasil/bot/service/cache/producer/DefaultCacheProducer.java
@@ -0,0 +1,54 @@
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2017 JBug:Brasil <contato@jbugbrasil.com.br>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package br.com.jbugbrasil.bot.service.cache.producer;
+
+import br.com.jbugbrasil.bot.service.cache.qualifier.DefaultCache;
+import br.com.jbugbrasil.bot.service.cache.qualifier.KarmaCache;
+import org.infinispan.cdi.embedded.ConfigureCache;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Logger;
+
+@ApplicationScoped
+public class DefaultCacheProducer {
+
+    private Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+
+    @Produces
+    @ConfigureCache("default-cache")
+    @DefaultCache
+    public Configuration specialCacheCfg(InjectionPoint injectionPoint) {
+        log.info("Configurando karma-cache...");
+        return new ConfigurationBuilder()
+                .indexing()
+                .autoConfig(true)
+                .addProperty("default.directory_provider", "ram")
+                .build();
+    }
+}
